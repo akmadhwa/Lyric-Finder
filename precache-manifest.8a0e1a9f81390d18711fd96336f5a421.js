9b6fd9aadafb3300531d5c65a831652c1be15d56
Muhammad Akma Adhwa <muhdakma63@gmail.com>
2019-02-04 18:40:26 +0800

    Updates

---
+++ precache-manifest.8a0e1a9f81390d18711fd96336f5a421.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6bd368ef176d8125d94",
+    "url": "/Lyric-Finder/static/js/runtime~main.c6bd368e.js"
+  },
+  {
+    "revision": "d05223b38a94c48235f0",
+    "url": "/Lyric-Finder/static/js/main.d05223b3.chunk.js"
+  },
+  {
+    "revision": "829cf7ad5df4034ea8e1",
+    "url": "/Lyric-Finder/static/js/1.829cf7ad.chunk.js"
+  },
+  {
+    "revision": "d05223b38a94c48235f0",
+    "url": "/Lyric-Finder/static/css/main.58dd30b2.chunk.css"
+  },
+  {
+    "revision": "abf447146217bb8f44eeacd3645166f9",
+    "url": "/Lyric-Finder/index.html"
+  }
+];
